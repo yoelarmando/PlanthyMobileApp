6add6d3da0cefb139c1534cfb7178244b22b7191
yoelarmando <105413682+yoelarmando@users.noreply.github.com>
2022-06-29 14:01:51 +0700

    Adding main feature UI

---
+++ PlanthyApp/app/src/main/java/com/example/planthyapp/LoginActivity.kt
@@ -0,0 +1,54 @@
+package com.example.planthyapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.planthyapp.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth : FirebaseAuth
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = Firebase.auth
+
+        binding.btnLogin.setOnClickListener {
+            setLogin(auth)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val currentUser = auth.currentUser
+
+        if(currentUser != null){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun setLogin(auth: FirebaseAuth){
+
+        val email = binding.emailEditText.text.toString()
+        val password = binding.passwordEditText.text.toString()
+
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+            if(it.isSuccessful) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }else {
+                Toast.makeText(this@LoginActivity, "Error Login", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
--- PlanthyApp/app/src/main/java/com/example/planthyapp/MainActivity.kt
+++ PlanthyApp/app/src/main/java/com/example/planthyapp/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.example.planthyapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.planthyapp.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var auth : FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = Firebase.auth
+
+        binding.btnPredict.setOnClickListener {
+            auth.signOut()
+            val intent = Intent(this , LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 
